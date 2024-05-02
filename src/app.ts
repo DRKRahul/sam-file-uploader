131830f298eebacd5e24fbda62dee3417047b146
Rahulkumar Daga <rahuldaga2009@gmail.com>
2024-05-02 22:58:19 +0530

    handler function

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,54 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, SQSEvent } from 'aws-lambda';
 
+const s3 = new AWS.S3();
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @param {APIGatewayProxyEvent} event - API Gateway Lambda Proxy Input Format
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * @returns {APIGatewayProxyResult} object - API Gateway Lambda Proxy Output Format
  *
  */
-
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const fileHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        console.log("-------------")
+        const body = JSON.parse(event.body || '');
+        const fileContent = body.fileContent;
+        const fileName = body.fileName;
+        console.log(process.env, "----------------------------");
+        // Define S3 bucket and key
+        const bucketName = process.env.FILES_BUCKET_NAME ?? '';
+        const key = `uploads/${fileName}`;
+    
+        // Upload file to S3
+        const params = {
+          Bucket: bucketName,
+          Key: key,
+          Body: fileContent,
+          ContentType: 'application/octet-stream'
+        };
+    
+        await s3.upload(params).promise();
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'File uploaded successfully' })
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
+
+export const handler = async (event: SQSEvent) : Promise<APIGatewayProxyResult> => {
+    try {
+        console.log("---------------------")
         return {
             statusCode: 200,
             body: JSON.stringify({
